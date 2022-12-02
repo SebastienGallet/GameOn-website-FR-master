cab8ce5db11226726122f341163819cd0681e707
Sebastien Gallet <sgallet.dev@gmail.com>
2022-12-02 15:08:02 +0100

    function validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.querySelector('form');
+const sendButton = document.querySelector('.btn-submit');
+const firstName = document.querySelector('#first');
+const lastName = document.querySelector('#last');
+const mail = document.querySelector('#email');
+const birthDate = document.querySelector('#birthdate');
+const tournamentQuantity = document.querySelector('#quantity');
+const localisationChoice = document.querySelector('.checkbox-input');
+const thanks = document.querySelector(".thanks");
+const regexName = /^[a-zA-ZéèîïÉÊÈÍÎÌÏ][a-zéèêëçïî]+([-'\s][a-zA-ZéèîïÉÊÈÍÎÌÏ][a-zéèêëçïî]+)?/;
+const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regexDate = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+const closeModal = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,161 +33,120 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
-//Selection de la croix de fermeture du popup
-const closeModal = document.querySelector(".close");
 //Fonction pour fermer le popup
 closeModal.addEventListener('click', function() {
   modalbg.style.display = "none";
 });
 
 
+
 //-----------------------------------------------------
 //  Vérification
 //-----------------------------------------------------
 
-const form = document.querySelector('form');
-const sendButton = document.querySelector('.btn-submit');
-const firstName = document.querySelector('#first');
-const firstNameError = document.querySelector('#firstNameError');
-const lastName = document.querySelector('#last');
-const lastNameError = document.querySelector('#nameError');
-const textControl = document.querySelector('.text-control');
-const mail = document.querySelector('#email');
-const mailError = document.querySelector('#mailError');
-const birthDate = document.querySelector('#birthDate');
-const dateError = document.querySelector('#dateError');
-const tournamentQuantity = document.querySelector('#quantity');
-const tournamentError = document.querySelector('#tournamentError');
-const localisationChoice = document.querySelector('#checkbox-input');
-const localisationError = document.querySelector("#localisationError");
-const thanks = document.querySelector(".thanks");
-let valid = false;
-
-
-
-const regexName = /^[a-zA-ZéèîïÉÊÈÍÎÌÏ][a-zéèêëçïî]+([-'\s][a-zA-ZéèîïÉÊÈÍÎÌÏ][a-zéèêëçïî]+)?/;
-const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
-
-// Vérification des valeurs
-document.querySelector('.text-control').addEventListener('input', isFormValid);
-
-function isFormValid(){
-  validationFirstname();
+function validate(){
+  if (
+    checkFirstName() &&
+    checkLastName() &&
+    checkMail() &&
+    checkBirth() &&
+    checkTournament() === true
+   // checkLocalisation()
+    ) {
+    showSendButton();
+    return true;
+  }
+  return false
 }
 
+document.querySelector('.text-control').addEventListener('input', showSendButton)
+function showSendButton() {
+  sendButton.style.opacity = "1";
+  sendButton.style.cursor= "pointer"
+}
 
-// Validation globale
-function formValid(e) {
+sendButton.addEventListener('click', function(e) {
   e.preventDefault();
-  validationFirstname();
-  validationName();
-  validationMail();
-  validationBirth();
-  validationTournament();
-  validationLocalisation();
-
-  if (valid = true) {
-    thanks.style.display = "block";
-    form.style.display = "none"
-    sendButton.style.opacity = "100%"
-    sendButton.style.cursor = 'pointer'
-  } else {
-    thanks.style.display = "none";
-    form.style.display = "block"
+  if(validate() === true) {
+    showValidationMessage()
   }
+})
+
+function showValidationMessage() {
+  form.style.display = "none";
+  thanks.style.display = "block"
 }
 
 // Prénom
-function validationFirstname() {
+firstName.addEventListener('input', checkFirstName)
+function checkFirstName() {
+  //retirer les espace
   if (firstName.value.length < 2) {
-    //retirer les espaces
     firstName.parentElement.setAttribute("data-error-visible", "true");
+    return false
   } else {
     firstName.parentElement.setAttribute("data-error-visible", "false");
+    return true
   }
 };
 
 //Nom
-function validationName() {
-  if (lastName === "" || lastName.lenght < 2) {
-    lastNameError.style.display = "block";
-    lastNameError.style.border = "1px solid red";
-    valid = false;
-    console.log('erreur last')
+lastName.addEventListener('input', checkLastName)
+function checkLastName() {
+  if (lastName.value.length < 2) {
+    lastName.parentElement.setAttribute("data-error-visible", "true");
+    return false
   } else {
-    lastNameError.style.display= "none";
-    valid = true
+    lastName.parentElement.setAttribute("data-error-visible", "false");
+    return true
   }
 };
 
 //Email
-function validationMail() {
+mail.addEventListener('input', checkMail)
+function checkMail() {
   if (mail.value.match(regexMail)) {
-    mailError.style.display = "none";
-    valid = true
+    mail.parentElement.setAttribute("data-error-visible", "false");
+    return true
   } else {
-    mailError.style.display= "block";
-    valid = false
+    mail.parentElement.setAttribute("data-error-visible", "true");
+    return false
   }
 }
 
 //Date de naissance
-function validationBirth() {
+function checkBirth() {
   if (birthDate.value.match(regexDate)) {
-    dateError.style.display = "none";
-    valid = true
+    birthDate.parentElement.setAttribute("data-error-visible", "false");
+    return true
   } else {
-    dateError.style.display= "block";
-    valid = false
-    console.log('erreur date')
+    birthDate.parentElement.setAttribute("data-error-visible", "true");
+    return false
   }
 }
 
 //Nombre de participations
-function validationTournament() {
-  if (tournamentQuantity.value >= 0 && tournamentQuantity.value < 100) {
-    tournamentError.style.display = "block";
-    valid = true;
+tournamentQuantity.addEventListener('input', checkTournament)
+function checkTournament() {
+  if (tournamentQuantity.value >= 0 && tournamentQuantity.value < 100 && tournamentQuantity.value !== '') {
+    tournamentQuantity.parentElement.setAttribute("data-error-visible", "false");
+    return true
   } else {
-    tournamentError.style.display = "none";
-    valid = false
-    console.log('erreur nbr tournois')
+    tournamentQuantity.parentElement.setAttribute("data-error-visible", "true");
+    return false
   }
 }
 
-// Choix d'un lieu
-function validationLocalisation() {
-  let check = false;
-  for (let i = 0; i < localisationChoice.length; i++) {
+//Choix d'un lieu
+/*function checkLocalisation() {
+  localisationChoice.setAttribute("data-error-visible", "true");
+  for (let i = 0; i < localisationChoice.lenght; i++) {
     if (localisationChoice[i].checked) {
-      check = true;
-      break;
+      localisationChoice.parentElement.setAttribute("data-error-visible", "false");
+      return true
     }
   }
-
-  if (check) {
-    localisationError.style.display = "none";
-    valid = true;
-  } else {
-    localisationError.style.display = "block"
-    valid = false
-    console.log('erreur localisation')
-  }
-};
-
-
-
-
-
-// const thanks = document.querySelector(".thanks");
-// const sendButton = document.querySelector(".btn-submit");
-// const formModal = document.querySelector(".reserve")
-
-// sendButton.addEventListener('click', function() {
-//   formModal.style.display = "none";
-//   thanks.style.display = "block";
-// })
+  return false
+};*/
 
 
